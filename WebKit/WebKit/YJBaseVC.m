c833feb3ff17dae2bc18a89c1cf787a8b6230188
yangjun <yangjun@touzila.com>
2015-11-30 13:45:15 +0800

    WebKit -> 基础功能完成，包含WKScriptMessageHandler和WKUIDelegate代理

--- WebKit/WebKit/YJBaseVC.m
+++ WebKit/WebKit/YJBaseVC.m
@@ -10,8 +10,11 @@
 //
 
 #import "YJBaseVC.h"
+#import <WebKit/WebKit.h>
 
-@interface YJBaseVC ()
+@interface YJBaseVC () <WKScriptMessageHandler, WKUIDelegate>
+
+@property (nonatomic, strong) WKWebView *webView; ///< WKWebView
 
 @end
 
@@ -19,22 +22,122 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    WKUserContentController *userContentController = [[WKUserContentController alloc] init];
+    [userContentController addScriptMessageHandler:self name:@"jsCallOC"];
+    
+    // WKWebView的配置
+    WKWebViewConfiguration *configuration = [[WKWebViewConfiguration alloc] init];
+    configuration.userContentController = userContentController;
+    
+    self.webView = [[WKWebView alloc] initWithFrame:self.view.frame  configuration:configuration];
+    [self.view addSubview:self.webView];
+    
+    self.webView.UIDelegate = self;
+    
+    // 找到index.html的路径
+    NSURL *url = [[NSBundle mainBundle] URLForResource:@"index" withExtension:@"html"];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url]; // url的位置
+    [self.webView loadRequest:urlRequest]; // 加载页面
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - WKScriptMessageHandler
+- (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message {
+    NSLog(@"方法名:%@", message.name);
+    NSLog(@"参数:%@", message.body);
+    // 方法名
+    NSString *methods = [NSString stringWithFormat:@"%@:", message.name];
+    SEL selector = NSSelectorFromString(methods);
+    // 调用方法
+    if ([self respondsToSelector:selector]) {
+        [self performSelector:selector withObject:message.body];
+    } else {
+        NSLog(@"未实行方法：%@", methods);
+    }
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark js调OC
+- (void)jsCallOC:(id)body {
+    if ([body isKindOfClass:[NSDictionary class]]) {
+        NSDictionary *dict = (NSDictionary *)body;
+        // oc调用js代码
+        NSString *jsStr = [NSString stringWithFormat:@"ocCallJS('%@')", [dict objectForKey:@"data"]];
+        [self.webView evaluateJavaScript:jsStr completionHandler:^(id _Nullable data, NSError * _Nullable error) {
+            if (error) {
+                NSLog(@"错误:%@", error.localizedDescription);
+            }
+        }];
+    }
 }
-*/
+
+#pragma mark - WKUIDelegate
+#pragma mark 新建webView
+- (WKWebView *)webView:(WKWebView *)webView createWebViewWithConfiguration:(WKWebViewConfiguration *)configuration forNavigationAction:(WKNavigationAction *)navigationAction windowFeatures:(WKWindowFeatures *)windowFeatures {
+     NSLog(@"%s",__FUNCTION__);
+    return webView;
+}
+
+#pragma mark 关闭webView
+- (void)webViewDidClose:(WKWebView *)webView {
+     NSLog(@"%s",__FUNCTION__);
+}
+
+#pragma mark alert弹出框
+/**
+ *  web界面中有弹出警告框时调用
+ *
+ *  @param webView           实现该代理的webview
+ *  @param message           警告框中的内容
+ *  @param frame             主窗口
+ *  @param completionHandler 警告框消失调用
+ */
+- (void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler {
+    NSLog(@"%s",__FUNCTION__);
+    // 确定按钮
+    UIAlertAction *alertAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler();
+    }];
+    // alert弹出框
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:message message:nil preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:alertAction];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+#pragma mark Confirm选择框
+- (void)webView:(WKWebView *)webView runJavaScriptConfirmPanelWithMessage:(nonnull NSString *)message initiatedByFrame:(nonnull WKFrameInfo *)frame completionHandler:(nonnull void (^)(BOOL))completionHandler {
+     NSLog(@"%s",__FUNCTION__);
+    // 按钮
+    UIAlertAction *alertActionCancel = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        // 返回用户选择的信息
+        completionHandler(NO);
+    }];
+    UIAlertAction *alertActionOK = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler(YES);
+    }];
+    // alert弹出框
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:message message:nil preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:alertActionCancel];
+    [alertController addAction:alertActionOK];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
+#pragma mark TextInput输入框
+- (void)webView:(WKWebView *)webView runJavaScriptTextInputPanelWithPrompt:(nonnull NSString *)prompt defaultText:(nullable NSString *)defaultText initiatedByFrame:(nonnull WKFrameInfo *)frame completionHandler:(nonnull void (^)(NSString * _Nullable))completionHandler {
+     NSLog(@"%s",__FUNCTION__);
+    // alert弹出框
+    __block UIAlertController *alertController = [UIAlertController alertControllerWithTitle:prompt message:nil preferredStyle:UIAlertControllerStyleAlert];
+    // 输入框
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = defaultText;
+    }];
+    // 确定按钮
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        // 返回用户输入的信息
+        UITextField *textField = alertController.textFields.firstObject;
+        completionHandler(textField.text);
+    }]];
+    // 显示
+    [self presentViewController:alertController animated:YES completion:nil];
+}
 
 @end
